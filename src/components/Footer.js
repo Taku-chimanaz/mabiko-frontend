513458d6167285f449a3ab01bf50f90eb114d12c
Takudzwa L Chimanikire <tkchimanaz@gmail.com>
2022-09-09 22:56:04 +0200

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+
+import './../css/Footer.css';
+
+export const Footer = () => {
+  return (
+   
+    <footer>
+        <p>Mabiko Restaurant brought to you by Code Upgrade Technologies</p>
+    </footer>
+  )
+}
