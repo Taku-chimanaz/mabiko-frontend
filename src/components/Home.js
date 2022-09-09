de1a7c4834d7b88805c44bf43099cb32b9b69d1b
Takudzwa L Chimanikire <tkchimanaz@gmail.com>
2022-09-09 10:09:23 +0200

    moved: moved all components to their own folder

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import { Header } from "./Header";
+import { Main } from "./Main";
+import { Stats } from "./Stats";
+
+export const Home = () => {
+
+  return (
+    
+    <>
+        <Header/>
+        <Main/>
+        <Stats/>
+    </>
+  )
+}
+
---
+++ src/components/Stats.js
@@ -0,0 +1,29 @@
+import './css/Stats.css';
+
+export const Stats = () => {
+  return (
+     
+    <section className="stats">
+        <Stat img={"time.png"} statValue={"since 2020"}/>
+        <Stat img={"branches.png"} statValue={"24 branches"}/>
+        <Stat img={"star.png"} statValue={"4 star"}/>
+    </section>
+  )
+}
+
+const Stat = ({img, statValue}) => {
+
+    return (
+        <article className="stat">
+
+            <div className="stat-img-container">
+                <img src={`./${img}`} alt="clock" />
+            </div>
+
+            <p className="stat-value">
+                {statValue}
+            </p>
+
+        </article>
+    )
+}
