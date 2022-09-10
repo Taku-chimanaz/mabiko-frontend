3ed939006f571c2c21b92c221d036735c6688bb1
Takudzwa L Chimanikire <tkchimanaz@gmail.com>
2022-09-10 19:37:55 +0200

    add

--- src/components/Locations.js
+++ src/components/Locations.js
@@ -1,6 +1,14 @@
+import { useContext } from 'react';
 import './../css/Locations.css';
+import { HomeContext } from './Home';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import { useState } from 'react';
 
 export const Locations = () => {
+
+  const countries = useContext(HomeContext);
+
+
   return (
     
     <section className="locations">
@@ -13,30 +21,79 @@ export const Locations = () => {
         <div className="available-countries">
 
             <ul>
-              <li>Zimbabwe</li>
-              <li>Zimbabwe</li>
-              <li>Zimbabwe</li>
-              <li>Zimbabwe</li>
-              <li>Zimbabwe</li>
-              <li>Zimbabwe</li>
+            {
+               countries.length <= 0 ? <p>No countries available</p> :
+
+               countries.map(country => {
+                 
+                 return <li><LocationOnIcon/> {country.name}</li>
+               })
+            }
             </ul>
         </div>
 
-        <div className="search-for-branch">
+        <SearchBranch countries={countries}/>
 
-          <div className="search-container">
-            
-            <div className="search-input">
-              <input type="text" placeholder="Enter country to search"/>
-            </div>
-             <button className="search-btn">Search</button>
-          </div>
-
-          <div className="search-results">
-            <p>No branches to show at the moment</p>
-          </div>
-
-        </div>
+        
     </section>
+  )
+}
+
+const SearchBranch = ({countries}) => {
+
+  const [searchedText, setSearchText] = useState("");
+  const [searchedItems, setSearchedItems] = useState([])
+
+
+  const searchCountry = (e) => {
+      setSearchText(e.target.value);
+
+      countries.forEach(country => {
+
+        if(country.name.startsWith(searchedText) && searchedText.length !== 0){
+           setSearchedItems([...country.branches]);
+           console.log(searchedItems)
+        }
+
+      })
+  }
+  
+
+  return(
+    <div className="search-for-branch">
+
+      <div className="search-container">
+            
+        <div className="search-input">
+
+          <input 
+            type="text" 
+            placeholder="Enter country to search"
+            value={searchedText}
+            onChange={e => searchCountry(e)}
+          ></input>
+        </div>
+    
+        <button className="search-btn">Search</button>
+
+      </div>
+
+      <div className="search-results">
+        
+        <ul>
+        {
+           searchedItems.length <= 0 ? <p>No branches to show</p> :
+
+           searchedItems.map(branch => {
+              return (
+                <li>{branch}</li>
+              )
+           })
+        }
+        </ul>
+      </div>
+
+    </div>
+
   )
 }
