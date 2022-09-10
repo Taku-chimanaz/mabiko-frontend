9efc64340af866a5d0414b9a1641e7bade4a2071
Takudzwa L Chimanikire <tkchimanaz@gmail.com>
2022-09-10 16:38:17 +0200

    maded branches count dynamic by useContext

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,11 +1,28 @@
 import './../css/Stats.css';
+import { HomeContext } from './Home';
+import { useContext } from 'react';
 
 export const Stats = () => {
+
+    
+    const countries = useContext(HomeContext)
+    
+    let branches = 0
+
+    if(countries.length > 0){
+        countries.forEach(country => {
+            branches += country.branches.length;
+        })
+    }
+    
+
+    console.log(branches)
+    
   return (
      
     <section className="stats">
         <Stat img={"time.png"} statValue={"since 2020"}/>
-        <Stat img={"branches.png"} statValue={"24 branches"}/>
+        <Stat img={"branches.png"} statValue={`${branches} branches`}/>
         <Stat img={"star.png"} statValue={"4 star"}/>
     </section>
   )
