ff852d162017e366ecf4f6a0a5e4956cfd7bec9a
Takudzwa L Chimanikire <tkchimanaz@gmail.com>
2022-09-13 14:32:53 +0200

    jj

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,40 @@
 import './../App.css';
-
 import { Home } from './Home';
+import { CreateReservation} from './CreateReservation'
+import React, { useContext, createContext, useEffect, useState } from "react";
+import { getCountries } from '../js/getCountries';
+
+export const GlobalContext = createContext();
+
+
 function App() {
+
+  const [availableCountries, setAvailableCountries] = useState([]);
+  const [message, setMessage] = useState("Fetching Countries")
+  const [open, setOpen] = useState(false);
+
+  useEffect(()=>{
+
+    // open snackbar to tell use that you are fetching data;
+
+    setOpen(true)
+
+    // function to fetch all countries
+
+   
+
+   getCountries(setAvailableCountries, setOpen, setMessage);
+
+    
+
+ },[]);
+
+
   return (
-    <div className="App">
+    <GlobalContext.Provider value={{open, setOpen, message, setMessage, availableCountries, setAvailableCountries}}>
       <Home/>
-    </div>
+      <CreateReservation/>
+    </GlobalContext.Provider>
   );
 }
 
