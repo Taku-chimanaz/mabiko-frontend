a3af5e9d5807071ce6c6988d8c14cd99986914e2
Takudzwa L Chimanikire <tkchimanaz@gmail.com>
2022-09-20 21:56:49 +0200

    added the last routes to all routes

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { Home } from './components/Home';
 import { CreateReservation } from './components/CreateReservation';
 import { SecondStep } from './components/SecondStep';
 import { ThirdStep } from './components/ThirdStep';
+import { LastStep } from './components/LastStep';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -20,6 +21,7 @@ root.render(
         <Route path="/create-reservation" element={<CreateReservation/>}/>
         <Route path="/second-step" element={<SecondStep/>}/>
         <Route path="/third-step" element={<ThirdStep/>}/>
+        <Route path="/last-step" element={<LastStep/>}/>
       </Routes>
     </BrowserRouter>
 
